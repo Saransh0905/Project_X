95c6e5dba094851be1cb285b0ff3ffb38c019b3c
Saransh <b18191@students.iitmandi.ac.in>
2019-09-17 22:22:35 +0530

    added crawler

--- Project_X_srapy/Project_X_srapy/spiders/extract_data.py
+++ Project_X_srapy/Project_X_srapy/spiders/extract_data.py
@@ -2,11 +2,22 @@ import scrapy
 class Protease(scrapy.Spider):
     name = 'Protease'
     start_urls = [
-        'https://www.uniprot.org'
+        'https://www.uniprot.org/uniprot/Q3Y6B8https://www.uniprot.org/uniprot/?query=taenia+solium+protease&sort=score'
     ]
 
     def parse(self,response):
-         title = response.css('title').extract()
+         self.entry_list = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "entryID", " " ))]//a/@href')[1].extract()
+         new_url = 'https://www.uniprot.org' + 
          yield {
-             'title':title
-         }
+            'entry': self.entry_list
+        }        
+
+
+    '''def parse2(self,response):
+         title = response.css('title::text').extract()
+         GO_MF = response.css(".molecular_function a::text").extract()
+         #entry_list = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "entryID", " " ))]//a/@href').extract()
+         yield {
+         'title': title,
+         'GO_MF':GO_MF
+         }'''
